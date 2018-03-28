1c6fdc5f67cc549ec795b0560cbee6a12f7b5e2a
Horacio Alvarado <halvarado@modernizacion.gob.ar>
2018-03-28 00:05:38 -0300

    Add connect & disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ const path = require('path');
 const hbs = require('hbs');
 const _ = require('lodash');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // hbs.registerPartials(path.join(__dirname, '/vies/partials'));
 
@@ -14,8 +19,18 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, function () {
-    console.log('Started on port 3000');
+io.on('connection', function (socket) {
+    console.log('New user connected');
+
+    socket.on('disconnect', function () {
+        console.log('User was Disconnected');
+    })
+});
+
+
+
+server.listen(port, function () {
+    console.log('Started on port ' + port);
 });
 
 module.exports = {
